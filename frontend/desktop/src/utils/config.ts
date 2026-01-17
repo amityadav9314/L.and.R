1808af724cbb3f496c015d994f2c9237553d4958
AKY <aky001@zohomail.in>
2026-01-17 23:55:08 +0530

    feat: Configure Desktop API URL via .env

--- frontend/desktop/src/utils/config.ts
+++ frontend/desktop/src/utils/config.ts
@@ -1,4 +1,3 @@
 export const BYPASS_AUTH = false;
-// export const API_URL = `http://localhost:8080`;
-export const API_URL = `https://landr.aky.net.in`;
-export const GOOGLE_WEB_CLIENT_ID = "330800561912-pf7pdbfsfjicv9fe4lkkf1q130gg2952.apps.googleusercontent.com";
+export const API_URL = import.meta.env.VITE_API_URL || `http://localhost:8080`;
+export const GOOGLE_WEB_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "330800561912-pf7pdbfsfjicv9fe4lkkf1q130gg2952.apps.googleusercontent.com";
